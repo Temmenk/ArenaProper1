e56e2d2bdf14bc4e8c375a0482f5cd9d9ae38e84
Temmenk <temmenk@my.erau.edu>
2025-01-30 12:46:45 -0700

    build arena and add player moevement

---
+++ Assets/scripts/ItemBehavior.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class ItemBehavior : MonoBehaviour 
+ {
+       // 1
+     void OnCollisionEnter(Collision collision)
+     {
+         // 2
+         if(collision.gameObject.name == "Player")
+         {
+             // 3
+             Destroy(this.transform.parent.gameObject);
+
+             // 4
+             Debug.Log("Item collected!");
+         }
+     }
+ } 

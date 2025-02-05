fe2f76e8314fca1a6b2011f55a0aa854fa64be84
Temmenk <temmenk@my.erau.edu>
2025-02-04 19:23:33 -0700

    imporved game
    added jump, UI, shooting, and win

--- Assets/scripts/ItemBehavior.cs
+++ Assets/scripts/ItemBehavior.cs
@@ -2,17 +2,21 @@ using UnityEngine;
 
 public class ItemBehavior : MonoBehaviour 
  {
-       // 1
+     public GameBehavior gameManager;
+ 
+     void Start()
+     {               
+           gameManager = GameObject.Find("GameManager").GetComponent<GameBehavior>();
+     }
+ 
      void OnCollisionEnter(Collision collision)
      {
-         // 2
-         if(collision.gameObject.name == "Player")
+         if (collision.gameObject.name == "Player")
          {
-             // 3
              Destroy(this.transform.parent.gameObject);
-
-             // 4
              Debug.Log("Item collected!");
+ 
+             gameManager.Items += 1;
          }
      }
  } 
--- Assets/scripts/PlayerBehavior.cs
+++ Assets/scripts/PlayerBehavior.cs
@@ -4,18 +4,20 @@ public class PlayerBehavior : MonoBehaviour
 {
    public float moveSpeed = 10f;
    public float rotateSpeed = 75f;
- 
-   private float vInput;
-   private float hInput;
- 
-    // 1
+    public float jumpVelocity = 5f;
+    public float distanceToGround = 0.1f;
+    public LayerMask groundLayer;
+    public GameObject bullet;
+    public float bulletSpeed = 100f;
+    private float vInput;
+    private float hInput;
     private Rigidbody _rb;
+    private CapsuleCollider _col;
  
-    // 2
     void Start()
     {
-         // 3
          _rb = GetComponent<Rigidbody>();
+         _col = GetComponent<CapsuleCollider>();
      }
  
      void Update()
@@ -24,7 +26,6 @@ public class PlayerBehavior : MonoBehaviour
        hInput = Input.GetAxis("Horizontal") * rotateSpeed;
     
 
-       // 4
        this.transform.Translate(Vector3.forward * vInput * 
        Time.deltaTime);
        this.transform.Rotate(Vector3.up * hInput * Time.deltaTime);
@@ -32,18 +33,43 @@ public class PlayerBehavior : MonoBehaviour
      }
      void FixedUpdate()
     {
-        // 2
+        if(IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            _rb.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
+        }
         Vector3 rotation = Vector3.up * hInput;
 
-        // 3
         Quaternion angleRot = Quaternion.Euler(rotation *
             Time.fixedDeltaTime);
 
-        // 4
         _rb.MovePosition(this.transform.position +
             this.transform.forward * vInput * Time.fixedDeltaTime);
 
-        // 5
         _rb.MoveRotation(_rb.rotation * angleRot);
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject newBullet = Instantiate(bullet,
+                this.transform.position + new Vector3(1,0,0),
+                    this.transform.rotation) as GameObject;
+            
+            Rigidbody bulletRB =
+                newBullet.GetComponent<Rigidbody>();
+            
+            bulletRB.linearVelocity = this.transform.forward * 
+                                            bulletSpeed;
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        Vector3 capsuleBottom = new Vector3( _col.bounds.center.x,
+            _col.bounds.min.y, _col.bounds.center.z);
+
+        bool grounded = Physics.CheckCapsule(_col.bounds.center,
+            capsuleBottom, distanceToGround, groundLayer,
+                QueryTriggerInteraction.Ignore);
+
+        return grounded;
     }
  } 
